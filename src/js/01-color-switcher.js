e8c48f1851355a3233dd9c0a3350abfa06357399
MariaK1506 <93434705+MariaK1506@users.noreply.github.com>
2022-05-30 23:19:51 +0300

    add task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
 
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStartBtnClick() {
+  const addbackgroundColor = () => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  };
+  intervalId = setInterval(addbackgroundColor, 1000);
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+}
+
+function onStopBtnClick() {
+  //   console.log(intervalId);
+  clearInterval(intervalId);
+  refs.stopBtn.disabled = true;
+  refs.startBtn.disabled = false;
+}
